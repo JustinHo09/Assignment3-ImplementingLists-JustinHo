2725fc3ab75b82af24147367f520a5b402558b60
JustinHo09 <hojustin09@gmail.com>
2026-02-10 22:14:26 -0600

    I added three vairables, title, artist, and legnth and a  constructor to initilize those variables.
    I added three variables title, artist, and length to represent the songs's title, song's artist, and it's duration. Then I created a constructor that will initialize those variables.

--- src/Song.java
+++ src/Song.java
@@ -1,3 +1,13 @@
 public class Song {
+    public String title;
+    public String artist;
+    public double length;
+
+    public Song(String title, String artist, double length){
+        this.title = title;
+        this.artist = artist;
+        this.length = length;
+    }
+
     
 }
