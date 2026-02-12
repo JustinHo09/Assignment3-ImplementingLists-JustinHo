1b1ae139ad14ab32f0f46b58d6c95c92cfad6365
JustinHo09 <hojustin09@gmail.com>
2026-02-11 21:05:51 -0600

    I made an empty constructor, and a size, maxSize, and array of song variables.
    I declared three variables in the class, size, maxSize, and ABList. Size is the current size of the array, specifically the space currently being occupied with data. Maxsize is the maximum size of the array. ABList is just the array that hold all of the data. The constructor initilizes size to 0, maxsize to 4, and makes ABList a new song Array, which has a size equal to max size.

--- src/AList.java
+++ src/AList.java
@@ -1,3 +1,15 @@
 public class AList {
+
+    public int size;
+    public int maxSize;
+    public Song[] ABList;
+
+    public AList(){
+        size = 0;
+        maxSize = 4;
+        ABList = new Song[maxSize];
+    }
+
     
+
 }
