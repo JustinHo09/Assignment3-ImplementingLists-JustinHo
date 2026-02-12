cf3fec04ac6f655d60d0c254c1271bb23689f9ca
JustinHo09 <hojustin09@gmail.com>
2026-02-12 13:26:52 -0600

    I made a removy method that will remove a node at a given position.
    I made a removy method, which will remove a node a the given position. If the position is less than 0 is is not valid an message will print saying so. If it is 0 then that means the head will be removed, and in that case the head will be set to current's next. If the next is null then thats it, if not then the head's previous is set to null afterwars. Then if it is greater than 0, a loop will happen until the position is reached or current is null. If current is null than that means the position was not valid and a message will be printed saying so. If not, then it will set the current nodes' previous to current's next. If the node's next is not null it will set its previous to curren't sprevious. If it is null then that does not happen, Then curren'ts prevous and next are both set to null.

--- src/DLList.java
+++ src/DLList.java
@@ -20,4 +20,35 @@ public class DLList {
             node.setPrevious(current);
         }
     }
+
+    public void removy(int pos){
+        DLNode current = head;
+        if(pos < 0){
+            System.out.println("Not a valid position in the list");
+        }else if( pos == 0){
+            if(current.getNext() == null){
+                head = current.getNext();
+            }else{
+                head = current.getNext();
+                current.setNext(null);
+                head.setPrevious(null);
+            }
+        }else{
+            int counter=0;
+            while( counter < pos && current != null){
+                current=current.getNext();
+                counter++;
+            }
+            if(current == null){
+                System.out.println("Not a valid position in the list");
+            }else{
+                current.getPrevious().setNext(current.getNext());
+                if(current.getNext() != null){
+                    current.getNext().setPrevious(current.getPrevious());
+                }
+                current.setPrevious(null);
+                current.setNext(null);
+            }
+        }
+    }
 }
