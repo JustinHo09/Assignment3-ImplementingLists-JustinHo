64b1fd807c424c8d35047aa611750fa939e90a02
JustinHo09 <hojustin09@gmail.com>
2026-02-12 13:16:36 -0600

    I made an addy method to add a node with the song at the end of the list.
    I made an addy method that adds a node with the new song as its data to the end of the list. If the head is null, the node is just set to head. If the head is not null, then we make a new node called current and set it to head, and loop until current,next is null, meaning we are at the end. Then it will set current's next to the new node and set the new node's previous to current.

--- src/DLList.java
+++ src/DLList.java
@@ -5,5 +5,19 @@ public class DLList {
     public DLList(){
         head=null;
     }
-    
+
+    public void addy(Song s){
+        DLNode node = new DLNode();
+        node.setData(s);
+        if(head == null){
+            head = node;
+        }else{
+            DLNode current = head;
+            while(current.getNext() != null){
+                current = current.getNext();
+            }
+            current.setNext(node);
+            node.setPrevious(current);
+        }
+    }
 }
