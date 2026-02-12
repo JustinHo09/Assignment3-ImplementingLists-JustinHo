19af7c49d47d21ffaf001c18d4689af515244bd3
JustinHo09 <hojustin09@gmail.com>
2026-02-12 12:56:10 -0600

    I added a method removy to remove an element at an specified position from the list.
    I added a method removy to remove an element at the specified position of the list, and updates the pointers. This will create a node current equal to head. Then it will check to see if the position is greater than 0, if not it then checks if it is 0, if not then it prints a message letting the user know its not valid. If it is 0 then that means its removing the head to the head is set to the next node and the old head's next is set to null. If it is greater than 0, it will loop and update current until it either reaches position-1, the node before the one we want to remove, or  current is null. If current is null, then the position was not a valid one in the list. If not then it sets current to be the one before we want to remove and sets that next value to be the next of the one we want to remove. Thus removing any pointer to it in the list.

--- src/SLList.java
+++ src/SLList.java
@@ -19,4 +19,24 @@ public class SLList {
             current.setNext(test);
         }
     }
+    public void removy(int pos){
+        SLNode current=head;
+        if(pos >0){
+            int counter=0;
+            while(counter != pos-1 && current != null){
+                current = current.getNext();
+                counter++;
+            }
+            if(current == null){
+                System.out.println("No node exists at that position");
+            }else {
+                current.setNext(current.getNext().getNext());
+            }
+        } else if( pos == 0){
+            head = current.getNext();
+            current.setNext(null);
+        }else{
+            System.out.println("Not a valid position");
+        }
+    }
 }
