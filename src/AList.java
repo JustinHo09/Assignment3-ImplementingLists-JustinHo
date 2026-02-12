95257ed1a9732579b1f0d0d98989780ea0f7a900
JustinHo09 <hojustin09@gmail.com>
2026-02-11 21:25:27 -0600

    I added a toString method which will get a string containing all of the Songs in the list's toString results.
    I made this toString method to return a string that contains all of the string results of the Song's toString method but each on a different line. The different line part is done by adding "\n" at the end. This loops through the array and calls Song's toString method on add of them and concatinates it to the current output until it reaches the end. Then it returns the whole string.

--- src/AList.java
+++ src/AList.java
@@ -34,11 +34,19 @@ public class AList {
         if(pos <size-1 && pos >=0){
             for(int i=pos;i<size-1;i++){
                 ABList[i] = ABList[i+1];
-            } 
+            }
             size--;
         }else{
             System.out.println("Not a valid index in AList");
         }
+    }
+
+    public String toString(){
+        String output="";
+        for(int i=0; i<size; i++){
+            output= output+ABList[i].toString()+"\n";
+        }
+        return output;
     }
 
 }
