5f421161797163848d197d273a84bf5dfe7fe7b0
JustinHo09 <hojustin09@gmail.com>
2026-02-15 14:20:03 -0600

    I added a main method with five songs.
    I made a main method which will eventually run methods to test each list. I also created each list in the main method. I also added five songs to an array of Songs to use for testing.

--- src/Test.java
+++ src/Test.java
@@ -1,3 +1,27 @@
 public class Test {
+
+    public static void main(String[] args){
+        AList alistSong = new AList();
+        SLList slSong = new SLList();
+        DLList dlSong = new DLList();
+
+        Song [] songs = new Song[5];
+
+        Song song1 = new Song("Gira Gira", "Ado", 2.58);
+        Song song2 = new Song("Color Your Night",  "Atlus", 2.45);
+        Song song3 = new Song("New Genisis", "Ado", 3.00);
+        Song song4 = new Song("Whims of Fate", "Lyn", 4.25);
+        Song song5 = new Song("Virtual Insanity", "Jamiroquai", 3.55);
+
+        songs[0] = song1;
+        songs[1] = song2;
+        songs[2] = song3;
+        songs[3] = song4;
+        songs[4] = song5;
+
+    }
+
     
+
+
 }
