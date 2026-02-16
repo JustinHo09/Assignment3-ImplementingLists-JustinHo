1807781cf5a77e6f5f244c54ef0eda2865fd049d
JustinHo09 <hojustin09@gmail.com>
2026-02-15 18:21:28 -0600

    I added comments to the Test class and added another test.
    I added comments to the Test class. Then I added another test in each of them which is just adding an element after a removal.

--- src/Test.java
+++ src/Test.java
@@ -1,5 +1,15 @@
+/**
+ * This class is used to run tests on the three list classes made
+ * which are AList, SLList, and DLList.
+ */
 public class Test {
 
+    /**
+     *  This creates all three lists, AList, SLList, and DLList and
+     *  an array of songs then calls their respective test methods to
+     *  test them.
+     * @param args User input, but not used here.
+     */
     public static void main(String[] args){
         AList alistSong = new AList();
         SLList slSong = new SLList();
@@ -28,119 +38,160 @@ public class Test {
 
     }
 
-    public static void AListTest(AList list, Song[] songs){
+    /**
+     * This runs multiple tests on AList to test
+     * many different scenarios with its addy and removy methods
+     * and prints the list after each one. This is generic
+     * so that any array can be used as input.
+     *
+     * @param list The AList to test.
+     * @param t The array to use as data.
+     * @param <T> The generic type to make it fit that type.
+     */
+    public static <T> void AListTest(AList<T> list, T[] t){
         System.out.println("Now Testing AList");
 
-        System.out.println("Initial elements in AList (Should be empty)");
+        System.out.println("Initial elements in AList (Should be empty):");
         System.out.println(list.toString());
 
-        System.out.println("Removing an element from an empty list");
+        System.out.println("Removing an element from an empty list:");
         list.removy(0);
         System.out.println(list.toString());
 
-        System.out.println("Adding elements to reach max (4)");
+        System.out.println("Adding elements to reach max (4):");
         for(int i=0; i<4;i++){
-            list.addy(songs[i]);
+            list.addy(t[i]);
         }
         System.out.println(list.toString());
 
-        System.out.println("Adding an element after max");
-        list.addy(songs[4]);
+        System.out.println("Adding an element after max:");
+        list.addy(t[4]);
         System.out.println(list.toString());
 
-        System.out.println("Adding a duplicate(should be allowed)");
-        list.addy(songs[0]);
+        System.out.println("Adding a duplicate(should be allowed):");
+        list.addy(t[0]);
         System.out.println(list.toString());
 
-        System.out.println("Removing element at index 1");
+        System.out.println("Removing element at index 1:");
         list.removy(1);
         System.out.println(list.toString());
 
-        System.out.println("Removing an element at the end");
-        list.removy(4);
+        System.out.println("Adding an element after removal:");
+        list.addy(t[1]);
         System.out.println(list.toString());
 
-        System.out.println("Attempt removing at an index greater than size");
+        System.out.println("Removing an element at the end:");
+        list.removy(5);
+        System.out.println(list.toString());
+
+        System.out.println("Attempt removing at an index greater than size:");
         list.removy(6);
         System.out.println(list.toString());
         System.out.println("AList Test done");
         System.out.println();
     }
 
-    public static void SLListTest(SLList list, Song[] songs){
+    /**
+     * This method does many tests on a SLList by testing
+     * many scenarios with its addy and removy method.
+     * This then prints the result after each test and
+     * uses generics so it can take in any array as an input.
+     *
+     * @param list The SLList to test.
+     * @param t The array to use as data.
+     * @param <T> The generic type to make it fit that type.
+     */
+    public static <T> void SLListTest(SLList<T> list, T[] t){
         System.out.println("Now Testing SLList");
 
-        System.out.println("Initial elements in SLList (Should be empty)");
+        System.out.println("Initial elements in SLList (Should be empty):");
         System.out.println(list.toString());
 
-        System.out.println("Removing an element from an empty list");
+        System.out.println("Removing an element from an empty list:");
         list.removy(0);
         System.out.println(list.toString());
 
-        System.out.println("Adding elements to the list");
-        for(Song s: songs){
-            list.addy(s);
+        System.out.println("Adding elements to the list:");
+        for(int i = 0; i<t.length;i++){
+            list.addy(t[i]);
         }
         System.out.println(list.toString());
 
-        System.out.println("Adding a duplicate element(allowed)");
-        list.addy(songs[0]);
+        System.out.println("Adding a duplicate element(allowed):");
+        list.addy(t[0]);
         System.out.println(list.toString());
 
-        System.out.println("Removing an element that is not the head(position 1)");
+        System.out.println("Removing an element that is not the head(position 1):");
         list.removy(1);
         System.out.println(list.toString());
 
-        System.out.println("Removing the head");
+        System.out.println("Removing the head:");
         list.removy(0);
         System.out.println(list.toString());
 
-        System.out.println("Removing the last element");
-        list.removy(3);
+        System.out.println("Adding an element after removal:");
+        list.addy(t[1]);
         System.out.println(list.toString());
 
-        System.out.println("Attempt removing an element at an invalid position");
-        list.removy(3);
+        System.out.println("Removing the last element:");
+        list.removy(4);
+        System.out.println(list.toString());
+
+        System.out.println("Attempt removing an element at an invalid position:");
+        list.removy(4);
         System.out.println(list.toString());
 
         System.out.println("SLList Test Done");
         System.out.println();
     }
 
-    public static void DLListTest(DLList list, Song[] songs){
+    /**
+     * This method runs tests on DLList and prints out the
+     * list after each one. This is generic so that it can
+     * have any data as input to test.
+     *
+     * @param list The DLList to test.
+     * @param t The array with elements to use as data.
+     * @param <T> The generic type to make it fit that type.
+     */
+    public static <T> void DLListTest(DLList<T> list, T[] t){
         System.out.println("Now Testing DLList");
 
-        System.out.println("Initial elements in DLList(should be empty)");
+        System.out.println("Initial elements in DLList(should be empty):");
         System.out.println(list.toString());
 
-        System.out.println("Removing an element from an empty list");
+        System.out.println("Removing an element from an empty list:");
         list.removy(0);
         System.out.println(list.toString());
 
-        System.out.println("Adding elements to the list");
-        for(Song s:songs){
-            list.addy(s);
+        System.out.println("Adding elements to the list:");
+        for(int i = 0; i<t.length;i++){
+            list.addy(t[i]);
         }
         System.out.println(list.toString());
 
-        System.out.println("Adding a duplicate element(allowed)");
-        list.addy(songs[0]);
+        System.out.println("Adding a duplicate element(allowed):");
+        list.addy(t[0]);
         System.out.println(list.toString());
 
-        System.out.println("Removing an element that is not the head(position 1)");
+        System.out.println("Removing an element that is not the head(position 1):");
         list.removy(1);
         System.out.println(list.toString());
 
-        System.out.println("Removing an element at the end of the list");
+        System.out.println("Removing an element at the end of the list:");
         list.removy(4);
         System.out.println(list.toString());
 
-        System.out.println("Removing the head");
+        System.out.println("Adding an element after removal:");
+        list.addy(t[1]);
+        System.out.println(list.toString());
+
+        System.out.println("Removing the head:");
         list.removy(0);
         System.out.println(list.toString());
 
-        System.out.println("Attempting to remove an invalid position");
-        list.removy(3);
+        System.out.println("Attempting to remove an invalid position:");
+        list.removy(4);
         System.out.println(list.toString());
 
         System.out.println("DLList Test Done");
