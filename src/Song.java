65ebd07d94566fc05c82d371b2d57a0ffb5024c1
JustinHo09 <hojustin09@gmail.com>
2026-02-15 18:37:21 -0600

    I added comments to Song.
    I added comments to the Song class to give more detail about the class and its methods.

--- src/Song.java
+++ src/Song.java
@@ -1,16 +1,34 @@
+/**
+ * This is a class that represents a song in sportify and has
+ * three member data, title, artist, and length.
+ */
 public class Song {
     public String title;
     public String artist;
     public double length;
 
+    /**
+     * This constructor creates a new song and initializes all of its
+     * values to the parameters.
+     *
+     * @param title The title of the song.
+     * @param artist The song's artist.
+     * @param length The song's duration.
+     */
     public Song(String title, String artist, double length){
         this.title = title;
         this.artist = artist;
         this.length = length;
     }
 
+    /**
+     * This returns a string with all the songs information formated
+     * { title, artist, length}.
+     *
+     * @return A string with all the song's information.
+     */
     public String toString(){
-        String dataString = "{ Title :"+title+", Artist: "+artist+",Length: "+length+ " }";
+        String dataString = "{ Title :"+title+", Artist: "+artist+", Length: "+length+ " }";
         return dataString;
     }
 }
